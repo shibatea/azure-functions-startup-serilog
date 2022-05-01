84d8e6a6502a9aadbcecc4c21dfa04ca45e6829b
shibatea <ggl.takashi.68@gmail.com>
2022-05-01 11:56:10 +0900

    エラーログ出力確認用コードの追加

--- MyFunctionWithSerilog/MyFunctionWithSerilog/Function1.cs
+++ MyFunctionWithSerilog/MyFunctionWithSerilog/Function1.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Azure.WebJobs;
+﻿using System;
+
+using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 
 namespace MyFunctionWithSerilog;
@@ -10,6 +12,17 @@ public class Function1
     public Function1(ILogger<Function1> logger) => _logger = logger;
 
     [FunctionName("Function1")]
-    public void Run([QueueTrigger("myqueue-items-1", Connection = "")] string myQueueItem) =>
-        _logger.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
+    public void Run([QueueTrigger("myqueue-items-1", Connection = "")] string myQueueItem)
+    {
+        try
+        {
+            _logger.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
+
+            throw new ApplicationException("This is an application exception.");
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "catch exception.");
+        }
+    }
 }
